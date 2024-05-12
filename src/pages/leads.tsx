b98b44b42f2fff15b8010bbee8dfec9016f2f9c8
Natan Brito <natanismaelbrito@gmail.com>
2024-05-12 12:05:56 -0300

    fix: separating components by pages

---
+++ src/pages/leads.tsx
@@ -0,0 +1,27 @@
+import CommunityMembers from '../components/community-members/community-members'
+import Companies from '../components/companies/companies'
+import Footer from '../components/footer/footer'
+import Header from '../components/header/header'
+import Learn from '../components/learn/learn'
+import Modules from '../components/modules/modules'
+import Prices from '../components/prices/prices'
+import Teachers from '../components/teachers/teachers'
+import TechLeadsCommunity from '../components/tl-community/tl-community'
+
+function LeadsPage() {
+  return (
+    <main className='min-h-screen font-segoe font-bold'>
+      <Header />
+      <Learn />
+      <Modules />
+      <TechLeadsCommunity />
+      <Teachers />
+      <Companies />
+      <Prices />
+      <CommunityMembers />
+      <Footer />
+    </main>
+  )
+}
+
+export default LeadsPage
