e0ed245948a16eb9e945a27b1391f15b66622496
Natan Brito <natanismaelbrito@gmail.com>
2024-07-15 22:18:31 -0300

    feat: Made validation inputs fields and masks. Added a visual style for inputs fields in case of error

--- src/components/forms-sign-in/index.tsx
+++ src/components/forms-sign-in/index.tsx
@@ -1,23 +1,30 @@
-import { SyntheticEvent, useState } from 'react'
+import InputMask from 'react-input-mask'
+import { useForm, SubmitHandler } from 'react-hook-form'
 
 export default function LeftFirstPage() {
-  const [dataForm, setDataForm] = useState({
-    name: '',
-    email: '',
-    phone: '',
-  })
+  type IInputs = { name: string; email: string; phone: string }
 
-  const handleChangeValue = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setDataForm((dataForm) => ({
-      ...dataForm,
-      [event.target.name]: event.target.value,
-    }))
-  }
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IInputs>()
 
-  const handleSubmit = (event: SyntheticEvent) => {
-    event.preventDefault()
-    // Enviar pra API - salvar no Contexto
-    console.log(dataForm)
+  console.log(errors)
+
+  const onSubmit: SubmitHandler<IInputs> = (data) => {
+    const { name, email, phone } = data
+    const body: IInputs = {
+      name,
+      email,
+      phone: phone.replace(/[^0-9]/g, ''),
+    }
+    console.log(data)
+    console.log(body)
+    //#TODO  Decidir sobre o toastfy
+    //#TODO  Ver se posso aplicar a mascara no campo de numero
+    //#TODO  Mandar a Request de teste
+
   }
 
   return (
@@ -29,7 +36,7 @@ export default function LeftFirstPage() {
             voluptatibus non temporibus
           </div>
           <form
-            onSubmit={handleSubmit}
+            onSubmit={handleSubmit(onSubmit)}
             className='relative h-80 w-[70%] rounded-2xl bg-colorForms shadow-lg sm:w-[60%] md:h-80   lg:h-[24rem] lg:w-[70%]  xl:w-[65%] 2xl:h-[32rem] 2xl:w-[55%] '
           >
             <div className=' mb-8 flex-col pt-8 text-center text-12 lg:mb-12 lg:text-12  xl:mb-10 xl:text-15 2xl:mb-20 2xl:pt-12 2xl:text-15'>
@@ -40,36 +47,54 @@ export default function LeftFirstPage() {
             </div>
             <div className='mt-3 flex justify-center'>
               <input
-                name='name'
-                className='w-[85%]  rounded-md border-0 bg-grayBox p-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset   md:leading-5 lg:leading-7 2xl:leading-10'
+                {...register('name', { required: 'Name is required' })}
+                style={errors?.name ? { border: '2px solid red' } : {}}
+                className={
+                  'w-[85%] rounded-md   bg-grayBox p-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset   md:leading-5 lg:leading-7 2xl:leading-10'
+                }
                 placeholder='Nome'
-                onChange={handleChangeValue}
               />
             </div>
+
             <div className='mt-3 flex justify-center xl:mt-3 2xl:mt-6'>
               <input
-                name='email'
-                className='w-[85%] rounded-md border-0 bg-grayBox p-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset   md:leading-5 lg:leading-7 2xl:leading-10'
+                {...register('email', {
+                  required: 'Email is required',
+                  pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                })}
+                style={errors?.email ? { border: '2px solid red' } : {}}
+                className={
+                  'w-[85%] rounded-md  bg-grayBox p-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset   md:leading-5 lg:leading-7 2xl:leading-10'
+                }
                 placeholder='E-mail'
-                onChange={handleChangeValue}
               />
             </div>
+
             <div className='mt-3 flex justify-center xl:mt-3 2xl:mt-6'>
-              <input
-                name='phone'
+              <InputMask
+                maskPlaceholder={null}
+                mask='(99) 9 9999-999'
+                {...register('phone', {
+                  required: 'Phone is required',
+                  minLength: 11,
+                })}
+                style={errors?.phone ? { border: '2px solid red' } : {}}
                 className='w-[85%] rounded-md border-0 bg-grayBox p-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset   md:leading-5 lg:leading-7 2xl:leading-10'
                 placeholder='WhatsApp'
-                onChange={handleChangeValue}
               />
             </div>
+
             <div className='mt-6 flex justify-center lg:mt-8'>
               <button
                 type='submit'
-                className='flex w-[85%] justify-center rounded-md bg-blueButtonLight  px-3 py-1.5 text-sm font-semibold text-white shadow-sm transition-colors hover:bg-blueButtonMiddle   md:leading-6 lg:leading-7  2xl:leading-10'
+                className={
+                  'flex w-[85%] justify-center rounded-md bg-blueButtonLight  px-3 py-1.5 text-sm font-semibold text-white shadow-sm transition-colors hover:bg-blueButtonMiddle   md:leading-6 lg:leading-7  2xl:leading-10'
+                }
               >
                 GARANTIR MINHA VAGA
               </button>
             </div>
+            {/* <div className='mb-2  mr-5  h-[2rem] bg-blue-500  sm:h-[2.3rem] 2xl:h-[3rem] '></div> */}
             <div className='flex'>
               <div className='absolute bottom-[-1.2rem] right-[-2rem] mb-2  mr-5 h-8 w-8 rounded-lg bg-blue-500 sm:h-[2.3rem] sm:w-[2.3rem] 2xl:bottom-[-1.5rem] 2xl:right-[-2.3rem] 2xl:mr-6 2xl:h-[3rem] 2xl:w-[3rem] '></div>
               <div className='absolute bottom-1 right-[-2.3rem] mb-6 h-6 w-6 rounded-md bg-blue-500 2xl:right-[-3.5rem] 2xl:h-8 2xl:w-8'></div>
